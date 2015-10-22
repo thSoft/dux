cb87aa4b53a510b06a081eae48fc092ce4806b9d
Dénes Harmath <harmathdenes@gmail.com>
2015-10-23 01:52:31 +0200

    Refactor development scripts

--- scripts/paths.js
+++ scripts/paths.js
@@ -7,9 +7,11 @@ exports.root = path.join(path.dirname(process.argv[1]), '..');
 exports.elmPackage = path.join(exports.root, 'elm-package.json');
 var elmPackageContents = require(exports.elmPackage);
 
-exports.sourceDirectory = path.join(exports.root, elmPackageContents['source-directories'][0]);
+exports.sourceSubdirectory = elmPackageContents['source-directories'][0]
+exports.sourceDirectory = path.join(exports.root, exports.sourceSubdirectory);
 exports.sourceRootModule = path.join(exports.sourceDirectory, elmPackageContents['exposed-modules'][0]);
-exports.source = path.join(exports.sourceRootModule, 'Main.elm')
+exports.mainFilename = 'Main.elm';
+exports.source = path.join(exports.sourceRootModule, exports.mainFilename);
 
 exports.targetDirectory = path.join(exports.root, 'target');
 exports.target = path.join(exports.targetDirectory, 'elm.js')
@@ -21,9 +23,11 @@ exports.testDirectory = path.join(exports.root, 'test');
 exports.testElmPackage = path.join(exports.testDirectory, 'elm-package.json');
 var testElmPackageContents = require(exports.testElmPackage);
 
-exports.testSourceDirectory = path.join(exports.testDirectory, testElmPackageContents['source-directories'][0]);
+exports.testSourceSubdirectory = testElmPackageContents['source-directories'][0]
+exports.testSourceDirectory = path.join(exports.testDirectory, exports.testSourceSubdirectory);
 exports.testSourceRootModule = path.join(exports.testSourceDirectory, elmPackageContents['exposed-modules'][0]);
-exports.testSource = path.join(exports.testSourceRootModule, 'Test.elm')
+exports.testFilename = 'Test.elm';
+exports.testSource = path.join(exports.testSourceRootModule, exports.testFilename);
 
 exports.testTargetDirectory = path.join(exports.testDirectory, 'target');
 exports.testTarget = path.join(exports.testTargetDirectory, 'index.html');
